dfffbd9499e00cd76c4830b036e78037897f4278
Shaedow2000 <shaedow2000@proton.me>
2026-05-13 22:17:47 +0100

    Implement controller.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import type { Router } from "express";
 
-import { register, verifyAccount } from "../controller/auth.controller.ts";
+import {
+  register,
+  verifyAccount,
+  login,
+} from "../controller/auth.controller.ts";
 
 const AuthRouter: Router = express.Router();
 
 AuthRouter.post("/register", register);
 AuthRouter.post("/verify", verifyAccount);
+AuthRouter.post("/login", login);
 
 export default AuthRouter;
