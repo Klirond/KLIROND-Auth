5e220ee5d30369481032b2b259c1938db7ce7222
Shaedow2000 <shaedow2000@proton.me>
2026-05-13 22:17:05 +0100

    Make username optional.

--- src/global/zod.validation.object.ts
+++ src/global/zod.validation.object.ts
@@ -1,9 +1,10 @@
 import * as z from "zod";
 
 const AccountZodObject = z.object({
-  username: z.string().trim().min(4),
+  username: z.string().trim().min(4).optional(),
   email: z.email().trim().toLowerCase(),
-  password: z.string().min(6)
+  password: z.string().min(6),
 });
 
 export default AccountZodObject;
+
