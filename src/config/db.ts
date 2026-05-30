34005552169dffd4b809272a0d629a4edddedaee
Shaedow2000 <shaedow2000@proton.me>
2026-05-30 14:45:53 +0100

    Fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,9 @@ import logger from "../middlewares/logger.ts";
 const URI: string = process.env.URI ?? "";
 
 const connect: Function = async (): Promise<void> => {
-  await mongoose.connect(URI);
+  await mongoose.connect(URI, {
+    dbName: process.env.DB_NAME,
+  });
 
   logger.info("Connected to DataBase");
 };
